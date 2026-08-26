2106c9c4c1c0db246f3652a1553d6bfd7090a0a3
andrawing1212 <andrawing1212@gmail.com>
2026-08-26 23:29:01 +0900

    chore: wire temporary M-005 preview session postcheck

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,21 @@ import './index.css';
 import './components/studio/studioLayout.css';
 import './components/studio/tabletTouchPacedContent';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>,
-);
+const root = createRoot(document.getElementById('root')!);
+const params = new URLSearchParams(window.location.search);
+const isM005PreviewPostcheck = window.location.hostname === 'soridraw-music-git-preview-andrawing1212.vercel.app'
+  && params.get('m005-postcheck') === 'final-20260826';
+
+if (isM005PreviewPostcheck) {
+  void import('./m005PreviewSessionPostcheck').then(({ default: M005PreviewSessionPostcheck }) => {
+    root.render(<M005PreviewSessionPostcheck />);
+  });
+} else {
+  root.render(
+    <StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </StrictMode>,
+  );
+}

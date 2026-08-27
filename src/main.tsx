c3a47139339dd87e5962bebb7ef3a4dacd635ed3
andrawing1212 <andrawing1212@gmail.com>
2026-08-28 00:52:02 +0900

    fix: remove temporary Explore navigation bridge

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
-import ExploreNavigationBridge from './components/explore/ExploreNavigationBridge';
 import './index.css';
 import './components/studio/studioLayout.css';
 import './components/studio/tabletTouchPacedContent';
@@ -10,7 +9,6 @@ import './components/studio/tabletTouchPacedContent';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <ExploreNavigationBridge />
       <App />
     </Router>
   </StrictMode>,

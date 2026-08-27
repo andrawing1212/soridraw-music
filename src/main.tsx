ba3c70c30a1ad235f55a4e3184e720a709061f77
andrawing1212 <andrawing1212@gmail.com>
2026-08-28 00:08:38 +0900

    fix: keep App shell fixed on Explore route

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
 import ExploreNavigationBridge from './components/explore/ExploreNavigationBridge';
-import ExploreShell from './components/explore/ExploreShell';
 import './index.css';
 import './components/studio/studioLayout.css';
 import './components/studio/tabletTouchPacedContent';
 
-function SoridrawRoot() {
-  const location = useLocation();
-  const isExploreRoute = location.pathname === '/explore';
-
-  return (
-    <>
-      <ExploreNavigationBridge />
-      {isExploreRoute ? <ExploreShell /> : <App />}
-    </>
-  );
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <SoridrawRoot />
+      <ExploreNavigationBridge />
+      <App />
     </Router>
   </StrictMode>,
 );
